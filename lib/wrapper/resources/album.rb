dd09d20da9d967a1f5d42a57afd948da3a6a5404
tomallen400 <tomallen4000@gmail.com>
2014-02-11 11:01:23 +0000

    initialize tracks on albu

--- lib/wrapper/resources/album.rb
+++ lib/wrapper/resources/album.rb
@@ -6,12 +6,13 @@ class Decibel::Album
 		args.each do |k, v|
   		instance_variable_set("@#{k}", v) unless v.nil?
   	end
-  	#self.child_initializers
+  	self.child_initializers
 	end
 	
 	def child_initializers
 		self.identifiers_initialize if self.Identifiers
 		self.genres_initialize if self.Genres
+		self.tracks_initialize if self.Tracks
 	end
 	
 	def identifiers_initialize
@@ -35,7 +36,7 @@ class Decibel::Album
 		self.Tracks.each do |i|
 			array << Decibel::Recording.new(i)
 		end
-		self.Genres = array
+		self.Tracks = array
 	end
 
 end
